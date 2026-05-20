c5f9229a9aad4526483ab047c7a5a77d80f63330
Hardikwb <hghardikgupta1@gmail.com>
2026-05-20 12:15:38 +0530

    Project SetUp

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+
+function App() {  
+  return (
+    <>
+    <h1 className="bg-red-500">fnejn</h1>
+    <SignUp/>
+    <Login/>
+    </>
+  )
+}
+
+export default App
