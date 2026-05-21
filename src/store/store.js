ba6d9db5dcd63dc3ea555759f1f669532be7256b
Hardikwb <hghardikgupta1@gmail.com>
2026-05-21 17:25:40 +0530

    Configure Store Properly

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authService from "../appwrite/auth";
+import authSliceReducer from "./authSlice";
 
 const store = configureStore({
-    reducers:{
-        
+    reducer:{
+        authSlice:authSliceReducer,
     }
-})
+});
 
 export default store
