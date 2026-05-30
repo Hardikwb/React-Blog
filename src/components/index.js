7277ae43a38acc7594a95532dea514850a188c52
Hardikwb <hghardikgupta1@gmail.com>
2026-05-30 19:22:04 +0530

    Adding components

--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,30 @@
 import Header from "./Header/Header"
 import Footer from "./Footer/Footer"
-import Home from "./Home"
 import Container from "./container/Container"
 import LogoutBtn from "./Header/LogoutBtn"
 import Logo from "./Logo"
-
-
+import Input from "./Input"
+import Button from "./Button"
+import Select from './Select'
+import RealTimeEditor from "./RealTimeEditor"
+import SignUp from "./SignUp"
+import PostForm from "./post-form/PostForm"
+import Protected from "./ProtectedAuthLayout"
+import PostCard from "./PostCard"
+import Login from "./Login"
 export{
     Header,
     Footer,
-    Home,
     Container,
     LogoutBtn,
-    Logo
+    Logo,
+    Login,
+    Button,
+    Input,
+    Select,
+    RealTimeEditor,
+    SignUp,
+    PostForm,
+    PostCard,
+    Protected
 }
